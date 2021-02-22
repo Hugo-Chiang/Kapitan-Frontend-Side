7a5cf7704ddb779ffea04f220e85d747765bf5da
Hugo-Chiang <statesman1991@gmail.com>
2021-02-23 01:45:58 +0800

    first update

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view to="/store"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  created() {
+    const api = "https://be-sp-0001-kapitan.herokuapp.com/api/store.php";
+    const vm = this;
+
+    console.log(this);
+
+    this.$http.get(api).then((response) => {
+      console.log(response.data);
+    });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/pages/Store.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in response">{{ item["0"] }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      response: [],
+    };
+  },
+  created() {
+    const api = "https://be-sp-0001-kapitan.herokuapp.com/api/store.php";
+    const vm = this;
+
+    console.log(this);
+
+    this.$http.get(api).then((response) => {
+      console.log(response.data);
+      vm.response = response.data;
+    });
+  },
+};
+</script>
+
+<style>
+</style>
