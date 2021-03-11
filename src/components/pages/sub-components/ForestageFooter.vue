51d35e9feeee6f66505666d3e3ec3906efffc509
Hugo-Chiang <statesman1991@gmail.com>
2021-03-11 20:39:36 +0800

    完成頁腳骨架

--- src/components/pages/sub-components/ForestageFooter.vue
+++ src/components/pages/sub-components/ForestageFooter.vue
@@ -1,27 +1,273 @@
 <template>
+  <!-- 頁腳開始 -->
   <footer class=".container-fluid">
-    <i class="fab fa-facebook"></i>
-    <i class="fab fa-twitter"></i>
-    <i class="fab fa-instagram"></i>
-    <i class="fas fa-map-marker-alt"></i>251 新北市淡水區觀海路201號
-    <i class="fas fa-envelope"></i>service@Kapitan.com
-    <i class="fas fa-phone-alt"></i>(886)2-3345-6789 © 2035 Kapitan
+    <div class="container">
+      <div class="row px-5">
+        <div class="col-5">
+          <!-- 聯絡資訊清單開始 -->
+          <ul class="contact-info-list mt-3 ml-2">
+            <!-- 聯絡資訊項目開始 -->
+            <li v-for="(info, key) in contactInfo" :key="key">
+              <div
+                class="d-inline-block"
+                @click="contactInfo[key].onClickEvent"
+              >
+                <div
+                  class="contact-icon-block d-inline-flex justify-content-center align-items-center mb-4 mr-2"
+                >
+                  <i
+                    class="contact-icon"
+                    :class="contactInfo[key].iconClass"
+                  ></i>
+                </div>
+                <div
+                  class="contact-text-block d-inline-flex"
+                  @click="contactInfo[key].onClickEvent"
+                >
+                  {{ contactInfo[key].text }}
+                </div>
+              </div>
+            </li>
+            <!-- 聯絡資訊項目結束 -->
+          </ul>
+          <!-- 聯絡資訊清單結束 -->
+        </div>
+        <div class="col-7">
+          <div class="about-kapitan mb-5">
+            <h5>甲必丹的使命</h5>
+            <p>
+              「我希望來到這裡的每個人，能以最優雅的姿態、最愉快的體驗，繼承先祖們的冒險精神。感受深藏在我們基因中的『甲必丹』情懷。」
+              ── 甲必丹執行長 · 鄭賢之
+            </p>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <h6 class="mb-3">在社群網站上追蹤甲必丹</h6>
+              <ul class="community-link-list d-flex flex-row">
+                <li
+                  class="mr-4 d-flex justify-content-center align-items-center"
+                  v-for="(communityLink, key) in communityLinks"
+                  :key="key"
+                  @click="linkTo(communityLinks[key].link)"
+                >
+                  <i :class="communityLinks[key].iconClass"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="col-6">
+              <h6 class="mb-3">或訂閱我們的電子報</h6>
+              <div class="input-group mb-3">
+                <input
+                  type="email"
+                  class="form-control"
+                  placeholder="請輸入電子信箱..."
+                  aria-label="Recipient's username"
+                  aria-describedby="basic-addon2"
+                />
+                <div class="input-group-append">
+                  <button class="btn" type="button">點擊訂閱</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div class="row px-4">
+        <div class="col-6 d-flex align-items-center">
+          <h6
+            class="fake-copy-right"
+            @mouseenter="showFakeCopyRightDescription = true"
+            @mouseleave="showFakeCopyRightDescription = false"
+          >
+            © 2035 Kapitan. all rights reserved.
+            <span
+              class="fake-copy-right-des text-warning"
+              v-show="showFakeCopyRightDescription"
+              >（實際上並沒有，前端作品而已）</span
+            >
+          </h6>
+        </div>
+        <div class="col-3"></div>
+        <div class="col-3">
+          <ul class="mass-links-list d-flex flex-row justify-content-end">
+            <li
+              class="position-relative"
+              @mouseenter="showFakePrivacyPolicy = true"
+              @mouseleave="showFakePrivacyPolicy = false"
+            >
+              隱私權政策
+              <div
+                v-if="showFakePrivacyPolicy"
+                class="reminder-bubble position-absolute d-flex justify-content-center align-items-center"
+              >
+                僅做樣式，無效果
+              </div>
+            </li>
+            <li
+              class="ml-3"
+              @click="linkTo('https://www.104.com.tw/jobs/main/')"
+            >
+              加入甲必丹
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </footer>
+  <!-- 頁腳結束 -->
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      contactInfo: {
+        address: {
+          iconClass: ["fas", "fa-map-marker-alt"],
+          text: "251 新北市淡水區觀海路201號",
+          onClickEvent: function () {
+            window.location.href = "https://goo.gl/maps/HDg6TVKTjm6FDMmh8";
+          },
+        },
+        email: {
+          iconClass: ["fas", "fa-envelope"],
+          text: "service@Kapitan.com",
+          onClickEvent: function () {
+            window.open("mailto:service@Kapitan.com");
+          },
+        },
+        phone: {
+          iconClass: ["fas", "fa-phone-alt"],
+          text: "（886）2-3345-6789",
+          onClickEvent: function () {
+            window.open("tel:+886233456789");
+          },
+        },
+      },
+      communityLinks: {
+        facebook: {
+          iconClass: ["fab", "fa-facebook-f"],
+          link: "https://www.facebook.com/",
+        },
+        twitter: {
+          iconClass: ["fab", "fa-twitter"],
+          link: "https://twitter.com/",
+        },
+        instagram: {
+          iconClass: ["fab", "fa-instagram"],
+          link: "https://www.instagram.com/",
+        },
+        linkedinIn: {
+          iconClass: ["fab", "fa-linkedin-in"],
+          link: "https://www.linkedin.com/",
+        },
+      },
+      showFakeCopyRightDescription: false,
+      showFakePrivacyPolicy: false,
+    };
+  },
+  methods: {
+    // 方法：根據傳入網址前往該地
+    linkTo(link) {
+      window.location.href = link;
+    },
   },
 };
 </script>
 
+
 <style lang="scss" scoped>
+@import "../../../assets/all.scss";
+
+* {
+  color: $kapitan-white;
+  list-style: none;
+}
+
+ul {
+  padding: 0;
+}
+
+.btn {
+  color: $kapitan-white;
+  background-color: $kapitan-logo;
+  &:hover {
+    color: $scuba-blue;
+  }
+}
+
 footer {
-  border: 1px solid blue;
   width: 100%;
-  height: 200px;
-  background-color: #446da3;
+  height: 380px;
+  padding: 60px 0px;
+  background-color: $kapitan-footer;
+  .contact-info-list {
+    li {
+      list-style: none;
+      .contact-icon-block {
+        width: 45px;
+        height: 45px;
+        border-radius: 45px;
+        background-color: $kapitan-logo;
+        cursor: pointer;
+        .contact-icon {
+          width: 80%;
+        }
+      }
+      .contact-text-block {
+        color: orange;
+        font-size: 18px;
+        font-weight: 400;
+        cursor: pointer;
+      }
+    }
+  }
+  .community-link-list {
+    padding: 0%;
+    li {
+      width: 40px;
+      height: 40px;
+      border-radius: 10px;
+      background-color: $midnight;
+      font-size: 20px;
+      cursor: pointer;
+      .fa-instagram {
+        font-size: 25px;
+      }
+    }
+  }
+  .fake-copy-right-des {
+    font-size: 12px;
+  }
+  .mass-links-list {
+    li {
+      cursor: pointer;
+      .reminder-bubble {
+        width: 120px;
+        height: 30px;
+        background-color: $scuba-blue;
+        border-radius: 8px;
+        transform: translate3d(-50%, -50%, 0);
+        top: -90%;
+        left: 50%;
+        font-size: 12px;
+        color: $midnight;
+        &::after {
+          content: "";
+          width: 0;
+          height: 0;
+          border-style: solid;
+          border-width: 10px 10px 0 10px;
+          border-color: $scuba-blue transparent transparent;
+          z-index: 1;
+          position: absolute;
+          transform: translate3d(-50%, -50%, 0);
+          top: 100%;
+          left: 50%;
+        }
+      }
+    }
+  }
 }
 </style>
--- src/components/pages/sub-components/ForestageNavbar.vue
+++ src/components/pages/sub-components/ForestageNavbar.vue
@@ -1,59 +1,80 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light .container-fluid">
-    <router-link class="nav-link mr-2" to="/">
-      <img id="logoImg" src="../../../assets/img/logo.png" alt="" />
-    </router-link>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <router-link class="nav-link" to="/About">關於甲必丹</router-link>
-        </li>
-        <li class="nav-item active">
-          <router-link class="nav-link" to="/Store">挑選航程</router-link>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input
-          class="form-control mr-sm-2"
-          type="search"
-          placeholder="請輸入搜尋內容..."
-          aria-label="Search"
-        />
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-          搜尋
-        </button>
-      </form>
-      <ul class="navbar-nav ml-4">
-        <router-link class="nav-link" to="/Cart"
+  <!-- 導覽列開始 -->
+  <nav class="navbar navbar-expand-lg navbar-light container-fluid">
+    <div class="col-3 d-flex">
+      <!-- 商標（首頁）連結開始 -->
+      <router-link class="nav-link mr-2" to="/">
+        <img id="logo-img" src="../../../assets/img/logo.png" alt="" />
+      </router-link>
+      <!-- 商標（首頁）連結結束 -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <!-- 關於頁連結開始 -->
+          <li class="nav-item">
+            <router-link class="nav-link non-icon-link" to="/About"
+              >關於甲必丹</router-link
+            >
+          </li>
+          <!-- 關於頁連結結束 -->
+          <!-- 商品（方案）頁連結開始 -->
+          <li class="nav-item">
+            <router-link class="nav-link non-icon-link" to="/Store"
+              >挑選航程</router-link
+            >
+          </li>
+          <!-- 商品（方案）頁連結結束 -->
+        </ul>
+      </div>
+    </div>
+    <div class="col-8"></div>
+    <div class="col-1 d-flex justify-content-end">
+      <ul class="navbar-nav">
+        <!-- 購物車 icon 連結開始 -->
+        <router-link class="nav-link mr-1" to="/Cart"
           ><i class="fas fa-shopping-cart fa-lg"></i>
         </router-link>
+        <!-- 購物車 icon 連結結束 -->
+        <!-- 會員中心 icon 連結開始 -->
         <router-link class="nav-link" to="/Member-Centre">
           <i class="fas fa-user fa-lg"></i>
         </router-link>
+        <!-- 會員中心 icon 連結結束 -->
       </ul>
     </div>
   </nav>
+  <!-- 導覽列結束 -->
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+@import "../../../assets/all.scss";
+
 nav {
-  background-color: #189ebb;
-  #logoImg {
+  background-color: $kapitan-header;
+  #logo-img {
     width: 70px;
   }
-  router-link {
-    color: white;
+  .nav-link {
+    color: oldlace !important;
+  }
+  .non-icon-link {
+    font-size: 17px;
+    &:hover {
+      color: orange !important;
+    }
   }
   .fa-shopping-cart,
   .fa-user {
     cursor: pointer;
     &:hover {
-      color: white;
+      color: $kapitan-white;
     }
   }
 }
