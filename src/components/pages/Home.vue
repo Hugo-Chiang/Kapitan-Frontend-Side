7912e598cda13a4dc25031d4f96ef84b4091c718
Hugo-Chiang <statesman1991@gmail.com>
2021-04-07 01:16:46 +0800

    請專心點

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,71 +1,101 @@
 <template>
   <main id="home-page" class="container-fluid">
-    <section id="projects-carousel-area" class="position-relative">
-      <a href="" @click.prevent="aa(1)">上頁</a>
-      <ul
-        id="cards-list"
-        class="position-absolute d-flex justify-content-between"
+    <section id="chapter-carousel" class="position-relative mx-auto">
+      <div
+        class="carousel-controls left-arrow d-inline-block position-absolute"
+        @click="aa(1)"
       >
-        <!-- 方案卡片開始 -->
-        <router-link
-          v-for="(project, index) in carouselData.carouselProjectsArr"
-          :key="project['PROJECT_ID']"
-          :data-id="project['PROJECT_ID']"
-          :to="{
-            name: '方案',
-            params: {
-              selectedProjectID:
-                carouselData.carouselProjectsArr[index]['PROJECT_ID'],
-            },
-          }"
-          class="router-link d-block mx-lg-0 mx-md-1 mb-5"
+        <i class="fas fa-chevron-left"></i>
+      </div>
+      <div
+        class="carousel-controls right-arrow d-inline-block position-absolute"
+        @click="aa(-1)"
+      >
+        <i class="fas fa-chevron-right"></i>
+      </div>
+      <div id="projects-carousel-area" class="position-relative">
+        <ul
+          id="cards-list"
+          class="position-absolute d-flex justify-content-between"
         >
-          <li class="card" :key="project['PROJECT_ID']">
-            <img
-              class="card-img-top"
-              :src="
-                project['PROJECT_AVATAR_URL'] != null
-                  ? GlobalVariables.cloudUrlprefix +
-                    project['PROJECT_AVATAR_URL']
-                  : GlobalVariables.cloudUrlprefix +
-                    GlobalVariables.cloudNoImgUrl
-              "
-              alt="這裡是卡片頂圖"
-            />
-            <div class="card-body position-relative">
-              <h6 class="card-title">
-                {{ project["PROJECT_NAME"] }}
-              </h6>
-              <p class="card-text"></p>
-              <div class="row">
-                <div
-                  class="card-people-num col-6 pl-0 position-absolute d-flex align-items-center"
-                >
-                  <span class="d-inline-block mr-1">
-                    <i class="fas fa-users"></i>
-                  </span>
-                  <span class="d-inline-block">
-                    {{ project["PROJECT_MIN_NUM_OF_PEOPLE"] }} -
-                    {{ project["PROJECT_MAX_NUM_OF_PEOPLE"] }}
-                  </span>
-                </div>
-                <div class="card-price col-6 pr-0 text-right position-absolute">
-                  每人
-                  <span class="price">
-                    {{
-                      Number(project["PROJECT_ORIGINAL_PRICE_PER_PERSON"])
-                        | currency
-                        | dollarSign
-                    }}
-                  </span>
+          <!-- 方案卡片開始 -->
+          <router-link
+            v-for="(project, index) in carouselData.carouselProjectsArr"
+            :key="project['PROJECT_ID']"
+            :data-id="project['PROJECT_ID']"
+            :to="{
+              name: '方案',
+              params: {
+                selectedProjectID:
+                  carouselData.carouselProjectsArr[index]['PROJECT_ID'],
+              },
+            }"
+            class="router-link d-block mx-lg-0 mx-md-1 mb-5"
+          >
+            <li class="card" :key="project['PROJECT_ID']">
+              <img
+                class="card-img-top"
+                :src="
+                  project['PROJECT_AVATAR_URL'] != null
+                    ? GlobalVariables.cloudUrlprefix +
+                      project['PROJECT_AVATAR_URL']
+                    : GlobalVariables.cloudUrlprefix +
+                      GlobalVariables.cloudNoImgUrl
+                "
+                alt="這裡是卡片頂圖"
+              />
+              <div class="card-body position-relative">
+                <h6 class="card-title">
+                  {{ project["PROJECT_NAME"] }}
+                </h6>
+                <p class="card-text"></p>
+                <div class="row">
+                  <div
+                    class="card-people-num col-6 pl-0 position-absolute d-flex align-items-center"
+                  >
+                    <span class="d-inline-block mr-1">
+                      <i class="fas fa-users"></i>
+                    </span>
+                    <span class="d-inline-block">
+                      {{ project["PROJECT_MIN_NUM_OF_PEOPLE"] }} -
+                      {{ project["PROJECT_MAX_NUM_OF_PEOPLE"] }}
+                    </span>
+                  </div>
+                  <div
+                    class="card-price col-6 pr-0 text-right position-absolute"
+                  >
+                    每人
+                    <span class="price">
+                      {{
+                        Number(project["PROJECT_ORIGINAL_PRICE_PER_PERSON"])
+                          | currency
+                          | dollarSign
+                      }}
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
-        </router-link>
-        <!-- 方案卡片結束 -->
-      </ul>
-      <a href="" @click.prevent="aa(-1)">下頁</a>
+            </li>
+          </router-link>
+          <!-- 方案卡片結束 -->
+        </ul>
+        <div id="carousel-pagination-block" class="position-absolute">
+          <ul
+            id="carousel-pagination"
+            class="p-0 m-0 d-flex justify-content-between"
+          >
+            <li
+              v-for="(page, index) in 4"
+              :class="
+                Math.abs(carouselData.carouselCurrentOffSet) == index
+                  ? 'current-page'
+                  : ''
+              "
+              :key="index"
+            ></li>
+          </ul>
+        </div>
+      </div>
     </section>
   </main>
 </template>
@@ -76,8 +106,8 @@ export default {
     return {
       carouselData: {
         carouselProjectsArr: [],
-        carouselPagination: 2,
-        carouselMoveOnXaxis: 0,
+        carouselCurrentOffSet: 0,
+        carouselXaxisPixels: 0,
       },
     };
   },
@@ -102,26 +132,32 @@ export default {
   methods: {
     aa(para) {
       let routerLinks = document.querySelectorAll(".router-link");
-      let carouselProjectsArr = this.carouselData.carouselProjectsArr;
-      let Xaxis = this.carouselData.carouselMoveOnXaxis;
+      let offSet = this.carouselData.carouselCurrentOffSet;
 
-      this.carouselData.carouselMoveOnXaxis += 355 * para;
+      console.log(offSet);
 
-      for (const routerLink of routerLinks) {
-        routerLink.style.transform = `translate3d(${Xaxis}px, 0, 0)`;
+      if (offSet + para != -4 || offSet + para != 1) {
+        this.carouselData.carouselCurrentOffSet += para;
+        this.carouselData.carouselXaxisPixels = 355 * this.returnOffSet;
+        let Xaxis = this.carouselData.carouselXaxisPixels;
+
+        console.log(offSet);
+        console.log(Xaxis);
+
+        for (const routerLink of routerLinks) {
+          routerLink.style.transform = `translate3d(${Xaxis}px, 0, 0)`;
+        }
       }
+    },
+  },
+  computed: {
+    returnOffSet() {
+      let offSet = this.carouselData.carouselCurrentOffSet;
+      if (offSet < -3) this.carouselData.carouselCurrentOffSet = -3;
+      else if (offSet > 0) this.carouselData.carouselCurrentOffSet = 0;
+      else return this.carouselData.carouselCurrentOffSet;
 
-      // if (para == 1) {
-      //   this.carouselData.carouselMoveOnXaxis = 326.625 * para;
-
-      //   for (const routerLink of routerLinks) {
-      //     routerLink.style.transform = `translate3d(${Xaxis}px, 0, 0)`;
-      //   }
-
-      //   // let firstItem = carouselProjectsArr.slice(0, 1);
-      //   // this.carouselData.carouselProjectsArr = carouselProjectsArr.slice(1);
-      //   // this.carouselData.carouselProjectsArr.push(firstItem);
-      // }
+      return this.carouselData.carouselCurrentOffSet;
     },
   },
 };
@@ -139,65 +175,104 @@ export default {
     display: block;
     margin: auto;
   }
-  #projects-carousel-area {
-    width: 967px;
-    height: 400px;
-    border: 1px solid blue;
-    transform: translate3d(-50%, -50%, 0);
-    left: 50%;
-    overflow: hidden;
-    #cards-list {
-      border: 1px solid red;
-      width: 2032px;
-      padding: 0;
-      // left: 0%;
-      // @include media-breakpoint-down(md) {
-      //   padding: 0px 60px;
-      // }
-      // @include media-breakpoint-down(sm) {
-      //   padding: 0;
-      // }
-      .router-link {
-        width: 255px;
-        transition: transform 0.8s cubic-bezier(0.43, 0.195, 0.02, 1);
-        .card {
-          height: 22rem;
-          color: black;
-          box-shadow: 1px 1px 1px 0.5px rgba(0, 0, 0, 0.2);
-          img {
-            width: 100%;
-            height: 160px;
-            object-fit: cover;
-            @include border-top-radius($card-inner-border-radius);
-          }
-          .card-body {
-            height: 75%;
-            .card-title {
-              font-weight: 600;
+  #chapter-carousel {
+    // border: 1px solid black;
+    width: 1140px;
+    .carousel-controls {
+      font-size: 32px;
+      transform: translate3d(0%, -50%, 0);
+      top: 50%;
+      cursor: pointer;
+      &:hover {
+        opacity: 0.85;
+      }
+    }
+    .left-arrow {
+      left: 30px;
+    }
+    .right-arrow {
+      right: 30px;
+    }
+    #projects-carousel-area {
+      width: 967px;
+      height: 400px;
+      // border: 1px solid blue;
+      transform: translate3d(-50%, 0%, 0);
+      left: 50%;
+      overflow: hidden;
+      #cards-list {
+        // border: 1px solid red;
+        width: 2032px;
+        padding: 0;
+        // left: 0%;
+        // @include media-breakpoint-down(md) {
+        //   padding: 0px 60px;
+        // }
+        // @include media-breakpoint-down(sm) {
+        //   padding: 0;
+        // }
+        .router-link {
+          width: 255px;
+          transition: transform 0.8s cubic-bezier(0.43, 0.195, 0.02, 1);
+          .card {
+            height: 22rem;
+            color: black;
+            box-shadow: 1px 1px 1px 0.5px rgba(0, 0, 0, 0.2);
+            img {
+              width: 100%;
+              height: 160px;
+              object-fit: cover;
+              @include border-top-radius($card-inner-border-radius);
             }
-            .card-text {
-              height: 40%;
-              overflow: hidden;
-              white-space: nowrap;
-              text-overflow: ellipsis;
-            }
-            .card-people-num,
-            .card-price {
-              bottom: 20px;
-            }
-            .card-people-num {
-              left: 20px;
-              font-size: 14px;
-            }
-            .card-price {
-              right: 20px;
-              font-size: 14px;
-              .price {
-                font-size: 18px;
-                font-weight: 700;
-                color: darkred;
+            .card-body {
+              height: 75%;
+              .card-title {
+                font-weight: 600;
+              }
+              .card-text {
+                height: 40%;
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+              }
+              .card-people-num,
+              .card-price {
+                bottom: 20px;
+              }
+              .card-people-num {
+                left: 20px;
+                font-size: 14px;
+              }
+              .card-price {
+                right: 20px;
+                font-size: 14px;
+                .price {
+                  font-size: 18px;
+                  font-weight: 700;
+                  color: darkred;
+                }
               }
             }
+          }
+        }
+      }
+      #carousel-pagination-block {
+        // border: 1px solid black;
+        transform: translate3d(-50%, 0%, 0);
+        left: 50%;
+        bottom: 3%;
+        #carousel-pagination {
+          // border: 1px solid black;
+          width: 60px;
+          li {
+            list-style: none;
+            width: 10px;
+            height: 10px;
+            border-radius: 10px;
+            background-color: #dddddd;
+          }
+          .current-page {
+            background-color: black;
           }
         }
       }
